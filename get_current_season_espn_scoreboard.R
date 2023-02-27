9dd664c9e1943be4e8681e4c968527b4e830cfb6
tmking2002 <tmking4@ncsu.edu>
2023-02-27 18:51:43 -0500

    Trying again

--- get_current_season_espn_scoreboard.R
+++ get_current_season_espn_scoreboard.R
@@ -3,6 +3,6 @@ library(devtools)
 
 source_url("https://raw.githubusercontent.com/tmking2002/softballR/main/R/get_espn_season_scoreboard.R")
 
-scoreboard <- softballR::get_espn_season_scoreboard(2023)
+scoreboard <- get_espn_season_scoreboard(2023)
 
 saveRDS(object = scoreboard, file = "data/espn_scoreboard_2023.RDS")
