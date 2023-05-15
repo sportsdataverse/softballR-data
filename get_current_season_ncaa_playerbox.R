25665f3b0aae00fd8928beaacf428a4ef68b5b3c
tmking2002 <tmking4@ncsu.edu>
2023-05-14 23:12:28 -0400

    Update get_current_season_ncaa_playerbox.R

--- get_current_season_ncaa_playerbox.R
+++ get_current_season_ncaa_playerbox.R
@@ -76,34 +76,34 @@ get_hitting_box <- function(id){
 }
 
 get_fielding_box <- function(id){
-  
+
   raw <- glue::glue("https://stats.ncaa.org/contests/{id}/box_score") %>%
     readLines()
-  
+
   fielding_id <- raw[grep("\t   <a href=\"/game/box_score/", raw)] %>%
     stringr::str_remove_all("\t   <a href=\"/game/box_score/|\">Box Score </a>")
-  
+
   raw <- glue::glue("https://stats.ncaa.org/game/box_score/{fielding_id}?year_stat_category_id=15022") %>%
     rvest::read_html() %>%
     rvest::html_table()
-  
+
   first_team <- as.character(raw[[6]][1,1])
   second_team <- as.character(raw[[7]][1,1])
-  
+
   upd <- rbind(raw[[6]],raw[[7]]) %>%
     `names<-`(raw[[6]][2,]) %>%
     dplyr::filter(!(Player %in% c(first_team, second_team,"Player","Totals")))
-  
+
   upd$team <- ifelse(upd$Player %in% raw[[6]]$X1, first_team, second_team)
   upd[upd == ""] <- "0"
   upd[] <- lapply(upd, gsub, pattern="/", replacement="")
-  
+
   upd <- upd %>%
-    dplyr::mutate(across(3:12, as.numeric)) %>% 
+    dplyr::mutate(across(3:12, as.numeric)) %>%
     dplyr::mutate(game_id = id)
-  
+
   return(upd)
-  
+
 }
 
 curr_hitting_box_d1 <- readRDS("data/D1_hitting_box_scores_2023.RDS")
@@ -172,20 +172,20 @@ get_ncaa_pitcher_player_box <- function(game_id){
 }
 
 get_ncaa_fielding_player_box <- function(game_id){
-  
+
   i <<- i + 1
-  
+
   fielding <- try(get_fielding_box(game_id))
-  
+
   return(fielding)
-  
+
 }
 
 # Fielding box scores
 
 i <- 0
 
-box <- do.call(rbind, lapply(X = game_ids, FUN = get_ncaa_fielding_player_box))
+box <- do.call(rbind, lapply(X = game_ids_d1, FUN = get_ncaa_fielding_player_box))
 
 box <- box %>%
   filter(!str_detect(Player,"Error : Document is empty|subscript out of bounds|Timeout was reached")) %>%
