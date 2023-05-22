d8ade0d5f379a93f94ad42e91f2210c5d0e097d8
tmking2002 <tmking4@ncsu.edu>
2023-05-22 13:52:53 -0400

    Update get_current_season_ncaa_pbp.R

--- get_current_season_ncaa_pbp.R
+++ get_current_season_ncaa_pbp.R
@@ -62,7 +62,7 @@ ncaa_softball_pbp <- function(game_id){
   opponent_name <- df[1,3] %>% as.character()
   opponent_col <- 3
 
-  if(nrow(df) == 0) return(NULL)
+  if(is.null(df)) return(NULL)
   
   filtered <- df %>%
     dplyr::rename(team = team_col, opponent = opponent_col) %>%
